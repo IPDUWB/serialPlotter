c6b06f5847ae6c0fdcd6d2bb1ab0635b1f110c37
Bart Monhemius <bart4524@gmail.com>
2016-03-10 23:15:13 +0100

    serialPlotter: First push

---
+++ src/main/java/org/expo/sensorplotter/ChartController.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2016 Bart Monhemius.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.expo.sensorplotter;
+
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Random;
+import java.util.ResourceBundle;
+import java.util.Timer;
+import java.util.TimerTask;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart.Data;
+import javafx.scene.chart.XYChart.Series;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ToggleButton;
+import javafx.util.StringConverter;
+import jssc.SerialPort;
+import org.expo.serial.Serial;
+
+public class ChartController implements Initializable {
+    
+    private final static int MAX_SAMPLES = 200;
+    private final static int REFRESH_RATE = 150;
+    private int numOfSamples = 0;
+    
+    private Timer timer;
+    
+    private Series<Number, Number> sensorDataSeries;
+    private Serial serialPort;
+    
+    private Collection<Data<Number, Number>> dataCollection;
+    private Random ranData;
+    
+    @FXML
+    private NumberAxis gAxis;
+    @FXML
+    private NumberAxis timeAxis;
+    @FXML
+    private ToggleButton connButton;
+    @FXML
+    private ChoiceBox<String> serialDevBox;
+    @FXML
+    private LineChart<Number, Number> sensorChart;
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        ranData = new Random();
+        serialPort = new Serial("");
+        // Chart and chartdata
+        dataCollection = new ArrayList<>();
+        sensorDataSeries = new Series<>();
+        timeAxis.setTickLabelFormatter(new StringConverter<Number>(){
+            @Override
+            public String toString(Number t) {
+                return new SimpleDateFormat("HH:mm:ss").
+                    format(new Date(t.longValue()));
+            }
+            @Override
+            public Number fromString(String string) {
+                throw new UnsupportedOperationException("Not supported");
+            }
+        });
+        sensorDataSeries.setName("SensorData");
+        sensorChart.getData().add(sensorDataSeries);
+        // Serial port choicebpox
+        serialDevBox.getItems().setAll(Serial.getSerialPorts());
+        serialDevBox.getSelectionModel().selectFirst();
+        // Serial port connection button
+        serialPort.addListener((obs, oldVal, newVal) -> {
+            try {
+                dataCollection.add(
+                    new Data<>(System.currentTimeMillis(),
+                        Integer.valueOf(newVal.trim())));
+            }catch(NumberFormatException e) {
+                e.printStackTrace(System.err);
+            }
+            
+        });
+        connButton.setOnAction((ActionEvent event) -> {
+            if(connButton.isSelected()) {
+                serialPort.setSerialPort(serialDevBox.getValue());
+                if(!serialPort.open(SerialPort.BAUDRATE_9600)) {
+                    connButton.setSelected(false);
+                }
+            } else {
+                serialPort.close();
+            }
+        });
+        timer = new Timer("DataTimer");
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                synchronized(sensorDataSeries) {
+                    Platform.runLater(()-> {
+                        sensorDataSeries.getData().addAll(dataCollection);
+                        numOfSamples = sensorDataSeries.getData().size();
+                        if(numOfSamples > MAX_SAMPLES)
+                            sensorDataSeries.getData().remove(0, 
+                                    numOfSamples - MAX_SAMPLES);
+                        dataCollection.clear();
+                    });
+                }
+            }
+        }, 0, 50);
+    }    
+}
---
+++ src/main/java/org/expo/serial/Serial.java
@@ -0,0 +1,185 @@
+/*
+ * Copyright 2016 Bart Monhemius.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.expo.serial;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.beans.value.ChangeListener;
+import jssc.SerialPort;
+import jssc.SerialPortException;
+
+/**
+ *
+ * @author bart452
+ */
+public class Serial {
+    
+    private String serialPort;
+    private StringBuilder stringBuilder = new StringBuilder();
+    private SerialPort sp;
+    private final StringProperty data = new SimpleStringProperty("");
+    
+    /**
+     * Open a serialport
+     * @param port Device file to open e.g. "/dev/ttyUSB0"
+     */
+    public Serial(String port) {
+        serialPort = port;
+        Runtime.getRuntime().addShutdownHook((new Thread() {
+            @Override
+            public void run() {
+                close();
+            }
+        }));
+    }
+    
+    /**
+     * Change the serial device. If the serial device is already opened, it will
+     * be closed.
+     * @param serialPort 
+     */
+    public void setSerialPort(String serialPort) {
+        if(isOpen())
+            close();
+        this.serialPort = serialPort;
+    }
+    
+    /**
+     * Get the name of the currently opened port
+     * @return serialport devicefile name
+     */
+    public String getPortName() {
+        return serialPort;
+    }
+    
+    
+    /**
+     * Open the serialport and create a connection. This function creates a
+     * event listener that is triggerd when serial data is available
+     * @param baudrate  The baudrate to configure the serial device with
+     * @return true of the port is succesfully opened, false otherwise
+     */
+    public boolean open(int baudrate) {
+        try {
+            sp = new SerialPort(serialPort);
+            if(sp.openPort()) {
+                sp.setParams(baudrate, 
+                  SerialPort.DATABITS_8, 
+                  SerialPort.STOPBITS_1, 
+                  SerialPort.PARITY_NONE);
+                sp.setEventsMask(SerialPort.MASK_RXCHAR);
+                sp.addEventListener(event -> {
+                    if(event.isRXCHAR()) {
+                        try {
+                            stringBuilder.append(sp.readString());
+                            String s  = stringBuilder.toString();
+                            if(s.contains("\n")) {
+                                if(!s.substring(0, s.indexOf("\n")).equals(""))
+                                    data.set(s.substring(0, s.indexOf("\n")));
+                                stringBuilder = new StringBuilder();
+                            }
+                        } catch(SerialPortException e) {
+                            System.out.println("Failed to read serial data \n" + e);
+                        }
+                    }
+                });
+            }
+        } catch (SerialPortException e) {
+            System.err.println("Failed to open serialport \n" + e);
+            sp = null;
+        }
+        return (sp != null);
+    }
+    
+    /**
+     * Write a byte to the serialport. Serial device has to be opened
+     * @param data Bytes to write
+     */
+    public void writeByte(byte data) {
+        try {
+            sp.writeByte(data);
+        } catch(SerialPortException e) {
+            System.err.println("Failed to write byte \n" +  e);
+        }
+    }
+    
+    /**
+     * Write a byte array to the serialport. The device has to be open
+     * @param data Bytes to write
+     */
+    public void writeBytes(byte data[]) {
+        try {
+            sp.writeBytes(data);
+        } catch(SerialPortException e) {
+            System.err.println("Failed to write byte \n" +  e);
+        }
+    }
+    
+    /**
+     * Get the data from the serial device
+     * @return The data received
+     */
+    public String getData() {
+        return data.get();
+    }
+    
+    /**
+     * Add a callback when data is received
+     * @param listener The callback to trigger when data is received
+     */
+    public void addListener(ChangeListener<String> listener) {
+        data.addListener(listener);
+    }
+    
+    /**
+     * Test if the serial port is open
+     * @return true is open, false is closed
+     */
+    public boolean isOpen() {
+        return sp == null ? false : sp.isOpened();
+    }
+    
+    /**
+     * Close the serial device
+     */
+    public void close() {
+        if(sp != null) {
+            try {
+                sp.removeEventListener();
+                if(sp.isOpened())
+                    sp.closePort();
+            } catch(SerialPortException e) {
+                System.err.println("Failed closing serial \n" + e);
+            }
+        }
+        System.out.println("Closed serialport");
+    }
+    
+    public static ArrayList<String> getSerialPorts() {
+        return Arrays.asList(new File("/dev").listFiles())
+                .stream()
+                .filter((file) -> (file.getName().contains("USB") 
+                        || file.getName().contains("ACM")))
+                .map(File::getAbsolutePath)
+                .collect(Collectors.toCollection(ArrayList<String>::new));
+    }
+    
+}
