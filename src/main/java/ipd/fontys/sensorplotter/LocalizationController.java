b77305284c71b8e23f1095d60112d7ccdfae71e0
Milou <miloudong@hotmail.com>
2017-12-06 14:53:24 +0100

    get x and y coordinates of all crossings localizationcontroller

--- src/main/java/ipd/fontys/sensorplotter/LocalizationController.java
+++ src/main/java/ipd/fontys/sensorplotter/LocalizationController.java
@@ -16,15 +16,15 @@ public class LocalizationController implements Initializable {
     private int xNumOfSamples = 0;
     private final XYChart.Series<Number, Number> xDataSeries = new XYChart.Series<>();
     private final Collection<XYChart.Data<Number, Number>> xDataCollection = new CopyOnWriteArrayList<>();
-    private double r1Beacon = 18;
-    private double r2Beacon = 18;
-    private double r3Beacon = 25;
-    private double x1Beacon = 0;
-    private double y1Beacon = 0;
-    private double x2Beacon = 0;
-    private double y2Beacon = 6;
-    private double x3Beacon = 7;
-    private double y3Beacon = 0;
+    private double r1Beacon = 3.81;
+    private double x1Beacon = 1.01;
+    private double y1Beacon = 0.6;
+    private double r2Beacon = 0.79;
+    private double x2Beacon = 5.4;
+    private double y2Beacon = 1.8;
+    private double r3Beacon = 4.94;
+    private double x3Beacon = 3;
+    private double y3Beacon = 6;
     private double x1Tag;
     private double y1Tag;
     private double x2Tag;
@@ -53,32 +53,33 @@ public class LocalizationController implements Initializable {
             try {
                // System.out.println("New Value is" + newVal);
                 if (newVal.contains("x")) {
-                    r1Beacon = Double.valueOf(newVal.replaceAll("[a-z]", ""));
+                    //r1Beacon = Double.valueOf(newVal.replaceAll("[a-z]", ""));
                 }
                 if (newVal.contains("y")) {
-                    r2Beacon = Double.valueOf(newVal.replaceAll("[a-z]", ""));
+                    //r2Beacon = Double.valueOf(newVal.replaceAll("[a-z]", ""));
                 }
                 if (newVal.contains("z")) {
-                    r3Beacon = Double.valueOf(newVal.replaceAll("[a-z]", ""));
+                    //r3Beacon = Double.valueOf(newVal.replaceAll("[a-z]", ""));
                 }
                // System.out.println("test1" + r1Beacon);
               //  System.out.println("test2" + r2Beacon);
               //  System.out.println("test3" + r3Beacon);
                 value1Array = triangulation(y1Beacon,y2Beacon,x1Beacon,x2Beacon,r1Beacon,r2Beacon);
-                x1Tag = value1Array[0];
-                x2Tag = value1Array[1];
-                y1Tag = value1Array[2];
-                y2Tag = value1Array[3];
+                y1Tag = value1Array[0];
+                y2Tag = value1Array[1];
+                x1Tag = value1Array[2];
+                x2Tag = value1Array[3];
                 value2Array = triangulation(y1Beacon,y3Beacon,x1Beacon,x3Beacon,r1Beacon,r3Beacon);
-                x3Tag = value1Array[0];
-                x4Tag = value1Array[1];
-                y3Tag = value1Array[2];
-                y4Tag = value1Array[3];
+                y3Tag = value2Array[0];
+                y4Tag = value2Array[1];
+                x3Tag = value2Array[2];
+                x4Tag = value2Array[3];
 
-                System.out.println("c1" + x1Tag + y1Tag);
-                System.out.println("c2" + x2Tag + y2Tag );
-                System.out.println("c3" + x3Tag + y3Tag);
-                System.out.println("c4" + x4Tag + y4Tag);
+
+                System.out.println("c1 ( " + x1Tag + "," + y1Tag + " )");
+                System.out.println("c2 ( " + x2Tag + "," + y2Tag + " )");
+                System.out.println("c3 ( " + x3Tag + "," + y3Tag + " )");
+                System.out.println("c4 ( " + x4Tag + "," + y4Tag + " )");
 
 
 
@@ -111,9 +112,9 @@ public class LocalizationController implements Initializable {
             double A;
             double B;
             double C;
-            double a;
-            double b;
-            double c;
+            double y2;
+            double y1;
+            double y0;
 
             double [] array;
             array = new double[4];
@@ -126,25 +127,34 @@ public class LocalizationController implements Initializable {
             B = -2 * (x1Coordinate - x2Coordinate);
             C = (2 * y1Coordinate) - (2 * y2Coordinate);
 
-            a = C * C / B * B + 1;
-            b = ((2 * A * C / B * B) - (2 * C * x1Coordinate / B) - 2 * y1Coordinate);
-            c = (A * A) / (B * B) - (2 * A * x1Coordinate) / B + x1Coordinate * x1Coordinate + y1Coordinate * y1Coordinate - r1Coordinate * r1Coordinate;
+            y2 = (C * C) / (B * B) + 1;
+            y1 = (2 * A * C / (B * B)) - (2 * C * x1Coordinate )/ B - 2 * y1Coordinate;
+            y0 = (A * A) / (B * B) - (2 * A * x1Coordinate) / B + x1Coordinate * x1Coordinate + y1Coordinate * y1Coordinate - r1Coordinate * r1Coordinate;
 
             //Finding out the roots
 
-            double temp1 = Math.sqrt(b * b - 4 * a * c);
-            y1Tag = (-b + temp1) / (2 * a);
-            y2Tag = (-b - temp1) / (2 * a);
+            double D1 = y1 * y1 - 4 * y2 * y0;
+            if (D1 >= 0)
+            {
+                D1 = Math.sqrt(D1);
+                y1Tag = (-y1 + D1) / (2 * y2);
+                y2Tag = (-y1 - D1) / (2 * y2);
 
-            x1Tag = (C*y1Tag + A)/B;
-            x2Tag = (C*y2Tag + A)/B;
+                x1Tag = (C * y1Tag + A) / B;
+                x2Tag = (C * y2Tag + A) / B;
 
-           // System.out.println("The roots of the Quadratic Equation \"2x2 + 6x + 4 = 0\" are " + y1Tag + " and " + y2Tag);
-            array[0] = y1Tag;
-            array[1] = y2Tag;
-            array[2] = x1Tag;
-            array[3] = x2Tag;
-            return array;
+                // System.out.println("The roots of the Quadratic Equation \"2x2 + 6x + 4 = 0\" are " + y1Tag + " and " + y2Tag);
+                array[0] = y1Tag;
+                array[1] = y2Tag;
+                array[2] = x1Tag;
+                array[3] = x2Tag;
+                return array;
+            }
+            else
+            {
+                System.out.println("Circles do not cross!");
+                return array;
+            }
         }
 
 }
