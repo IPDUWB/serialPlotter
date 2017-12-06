a2c7368e04bd9bec9e9944849b55d878668e1dc8
Milou <miloudong@hotmail.com>
2017-12-06 09:42:24 +0100

    some try-outs in localizationcontroller

--- src/main/java/ipd/fontys/sensorplotter/LocalizationController.java
+++ src/main/java/ipd/fontys/sensorplotter/LocalizationController.java
@@ -2,29 +2,23 @@ package ipd.fontys.sensorplotter;
 
 import ipd.fontys.serial.Serial;
 import javafx.animation.AnimationTimer;
-import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.BubbleChart;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TextField;
-import javafx.util.StringConverter;
-
 import java.net.URL;
-import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class LocalizationController implements Initializable {
 
     private final static int MAX_SAMPLES = 40;
-    private final static int REFRESH_RATE = 150;
     private int xNumOfSamples = 0;
     private final XYChart.Series<Number, Number> xDataSeries = new XYChart.Series<>();
     private final Collection<XYChart.Data<Number, Number>> xDataCollection = new CopyOnWriteArrayList<>();
+    private double xValue;
+    private double yValue;
+    private double zValue;
     @FXML
     private BubbleChart<Number, Number> bubbleChart;
 
@@ -36,19 +30,23 @@ public class LocalizationController implements Initializable {
             try {
                 System.out.println("New Value is" + newVal);
                 if(newVal.contains("x")) {
-                    double x = Double.valueOf(newVal.replaceAll("[a-z]",""));
+                    xValue = Double.valueOf(newVal.replaceAll("[a-z]",""));
                 }
                 if (newVal.contains("y")){
-                    double y = Double.valueOf(newVal.replaceAll("[a-z]",""));
+                    yValue = Double.valueOf(newVal.replaceAll("[a-z]",""));
                 }
                 if (newVal.contains("z")){
-                    double z = Double.valueOf(newVal.replaceAll("[a-z]",""));
+                    zValue = Double.valueOf(newVal.replaceAll("[a-z]",""));
                 }
-
+                System.out.println("test" + xValue);
+                System.out.println("test" + yValue);
+                System.out.println("test" + zValue);
             } catch(NumberFormatException e) {
                 e.printStackTrace(System.err);
             }
         });
+
+
 
         xDataSeries.setName("Distance");
         bubbleChart.getData().add(xDataSeries);
@@ -64,6 +62,22 @@ public class LocalizationController implements Initializable {
                 xDataCollection.clear();
             }
         }.start();
+
+            /*double A;
+            double B;
+            double C;
+            A = C*C/B*B + 1;
+            B = ((2*A*C/B*B)-(2*C*xValue/B)-2*yValue);
+
+            //Finding out the roots
+            double temp1 = Math.sqrt(B * B - 4 * A * C);
+
+            double root1 = (-B +  temp1) / (2*A) ;
+            double root2 = (-B -  temp1) / (2*A) ;
+
+            System.out.println("The roots of the Quadratic Equation \"2x2 + 6x + 4 = 0\" are "+root1+" and "+root2);*/
+
+
 
     }
 
